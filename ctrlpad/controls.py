f60683c0282ac98f734cbc7b64768e3acd07f2c3
Martin Fiedler <keyj@emphy.de>
2024-08-06 23:21:21 +0200

    first actual controls (grid layout, button); not yet functional

---
+++ ctrlpad/controls.py
@@ -0,0 +1,128 @@
+import logging
+
+from .sdl import GLAppWindow
+from .renderer import Renderer
+
+
+class ControlEnvironment:
+    ref_scale = 1000
+
+    def __init__(self, window: GLAppWindow, renderer: Renderer):
+        self.window = window
+        self.renderer = renderer
+        self.update_scale()
+
+    def update_scale(self):
+        self.control_scale = min(self.window.vp_width, self.window.vp_height) / self.ref_scale
+
+    def scale(self, x):
+        return round(x * self.control_scale)
+
+
+class Control:
+    def __init__(self, **style):
+        self.style = style
+        self.state = None
+        self.children = []
+        self.geometry = (0,0,0,0)
+        self.invalidate_layout()
+
+    def get(self, key: str, default=None):
+        if self.state:
+            return self.style.get(self.state + '_' + key, self.style.get(key, default))
+        else:
+            return self.style.get(key, default)
+
+    def invalidate_layout(self):
+        self.layout_valid = False
+        for child in self.children:
+            child.invalidate_layout()
+
+    def layout(self, env: ControlEnvironment, x0: int, y0: int, x1: int, y1: int):
+        self.geometry = (x0, y0, x1, y1)
+        self.do_layout(env, x0, y0, x1, y1)
+        self.layout_valid = True
+
+    def draw(self, env: ControlEnvironment):
+        if not self.layout_valid:
+            self.layout(env, *self.geometry)
+        self.do_draw(env, *self.geometry)
+        for child in self.children:
+            child.draw(env)
+
+    def do_layout(self, env: ControlEnvironment, x0: int, y0: int, x1: int, y1: int):
+        pass
+
+    def do_draw(self, env: ControlEnvironment, x0: int, y0: int, x1: int, y1: int):
+        pass
+
+
+class GridLayout(Control):
+    def __init__(self, min_cells_x: int = 0, min_cells_y: int = 0, **style):
+        super().__init__(**style)
+        self.min_cells = (min_cells_x, min_cells_y)
+
+    def put(self, grid_pos_x: int, grid_pos_y: int, grid_size_x: int, grid_size_y: int, control: Control):
+        self.children.append(control)
+        control.grid_start_x = grid_pos_x
+        control.grid_start_y = grid_pos_y
+        control.grid_end_x = grid_pos_x + grid_size_x
+        control.grid_end_y = grid_pos_y + grid_size_y
+        return control
+
+    def do_layout(self, env: ControlEnvironment, x0: int, y0: int, x1: int, y1: int):
+        if not self.children: return
+
+        # determine grid size
+        maxx, maxy = self.min_cells
+        for child in self.children:
+            maxx = max(maxx, child.grid_end_x)
+            maxy = max(maxy, child.grid_end_y)
+
+        # compute cell size (cs*) and actual grid start
+        margin  = env.scale(self.get('margin',  0))  # outer margin 
+        padding = env.scale(self.get('padding', 0))  # padding between cells
+        csx = ((x1 - x0) - 2 * margin - (maxx - 1) * padding) // maxx + padding
+        csy = ((y1 - y0) - 2 * margin - (maxy - 1) * padding) // maxy + padding
+        if not self.get('rectangular'):
+            csx = csy = min(csx, csy)
+        x0 = (x0 + x1 - csx * maxx + padding) // 2
+        y0 = (y0 + y1 - csy * maxy + padding) // 2
+
+        # layout cells
+        for child in self.children:
+            child.layout(env,
+                x0 + csx * child.grid_start_x,
+                y0 + csy * child.grid_start_y,
+                x0 + csx * child.grid_end_x - padding,
+                y0 + csy * child.grid_end_y - padding)
+
+
+class TextControl(Control):
+    def __init__(self, text, **style):
+        super().__init__(**style)
+        self.text = text
+
+
+class Button(TextControl):
+    def do_layout(self, env: ControlEnvironment, x0: int, y0: int, x1: int, y1: int):
+        self.border = env.scale(self.get('border', 3))
+        self.shadow = env.scale(self.get('shadow', 15))
+        self.radius = env.scale(self.get('radius', 30))
+        self.text_layout = env.renderer.fit_text_in_box(
+            x0 + self.border * 1.5, y0 + self.border,
+            x1 - self.border * 1.5, y1 - self.border,
+            env.scale(self.get('size', 50)), self.text,
+            self.get('halign', 2), self.get('valign', 2))
+
+    def do_draw(self, env: ControlEnvironment, x0: int, y0: int, x1: int, y1: int):
+        env.renderer.outline_box(
+            x0,y0, x1,y1, self.border,
+            colorO=self.get('outline', "666"),
+            colorU=self.get('fill1', "aaa"),
+            colorL=self.get('fill2', "ccc"),
+            radius=self.radius,
+            shadow_offset=self.shadow*0.25,
+            shadow_blur=self.shadow,
+            shadow_grow=self.shadow)
+        env.renderer.fitted_text(self.text_layout, self.get('text', "000"))
--- ctrlpad_test.py
+++ ctrlpad_test.py
@@ -4,6 +4,8 @@ import logging
 from ctrlpad.sdl import GLAppWindow, Cursor
 from ctrlpad.opengl import gl
 from ctrlpad.renderer import Renderer
+from ctrlpad.controls import ControlEnvironment, GridLayout, Button
+
 
 class MyApp(GLAppWindow):
     def on_init(self):
@@ -13,15 +15,21 @@ class MyApp(GLAppWindow):
         gl.ClearColor(.12, .34, .56, .78)
         self.set_cursor(Cursor.Hand)
 
+        self.env = ControlEnvironment(self, self.renderer)
+        self.grid = GridLayout(16,4, margin=20, padding=15)
+        self.grid.put(0,0, 2,2, Button("PANIC BUTTON", disabled_fill1="777", disabled_fill2="999", disabled_text="444")).state = 'disabled'
+        self.grid.put(2,0, 2,2, Button("COOL"))
+        self.grid.layout(self.env, 0,0, self.vp_width, self.vp_height)
+
+    def on_resize(self):
+        logging.info("screen resized to %dx%d", self.vp_width, self.vp_height)
+        self.grid.layout(self.env, 0,0, self.vp_width, self.vp_height)
+
     def on_draw(self):
         gl.Clear(gl.COLOR_BUFFER_BIT)
         self.renderer.begin_frame(self.vp_width, self.vp_height)
 
         self.renderer.text(self.vp_width, self.vp_height, self.vp_height//4, "CAPTION", "0001", halign=1, valign=1)
-
-        self.renderer.outline_box(100, 100, 200, 200, 2, "def", "789", "abc", 20, shadow_offset=2, shadow_blur=8, shadow_grow=8)
-        #self.renderer.outline_box(100, 100, 200, 200, 2, "123", "789", "abc", 20)
-        self.renderer.text(150, 150, 25, "PANIC\nBUTTON", "000", halign=2, valign=2)
 
         self.renderer.box(400,100, 600,400, "fff4")
         self.renderer.text(400,100, 20, [l for l,w in self.renderer.wrap_text(200, 20, "This is a very long text, which I'd like to flow nicely into its box.")])
@@ -32,10 +40,13 @@ class MyApp(GLAppWindow):
         self.renderer.box(700,300, 800,400, "fff4")
         self.renderer.fitted_text(self.renderer.fit_text_in_box(700,300, 800,400, 20, "Slightly Longer Text Here (needs resizing)"))
 
+        self.grid.draw(self.env)
+
         self.renderer.end_frame()
 
     def on_key_down(self, sym: str):
         if sym == 'Q': self.quit()
+
 
 if __name__ == "__main__":
     logging.basicConfig(
