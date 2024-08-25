4a919506dde3f706d3231a1e460a0fc78f6536d3
Martin Fiedler <keyj@emphy.de>
2024-08-25 22:20:31 +0200

    add MPD (Music Player Daemon) client
    Comes with a control widget that displays artist/title/album
    metadata and has rudimentary controls (prev/next, play/pause,
    seek).
    
    The test application now seriously begins to look like something
    one could use at a demoparty.

---
+++ ctrlpad/mpd.py
@@ -0,0 +1,388 @@
+# SPDX-FileCopyrightText: 2024 Martin J. Fiedler <keyj@emphy.de>
+# SPDX-License-Identifier: MIT
+
+import logging
+import os
+import re
+import socket
+import threading
+import time
+
+from .renderer import Renderer
+from .controls import ControlEnvironment, Control
+from . import color
+
+###############################################################################
+
+class MPDClient:
+    "MPD (Music Player Daemon) client"
+
+    def __init__(self, ip: str = "localhost", port: int = 6600, timeout: float = 0.1, name: str = None):
+        self.log = logging.getLogger(name or ("MPD-" + ip))
+        self.ip = ip
+        self.port = port
+        self.timeout = timeout
+        self.sock = None
+        self.lock = threading.Lock()
+        self.cancel = False
+        self.async_cmds = []
+        self.async_quiet = False
+        self.async_result = None
+        self.async_trigger = threading.Event()
+        self.async_done = threading.Event()
+        self.async_done.set()
+        self.async_thread = threading.Thread(target=self._async_thread_func, name=self.log.name+"-AsyncThread")
+        self.async_thread.daemon = True
+        self.async_thread.start()
+        self.connect()
+
+    def __del__(self):
+        self.cancel = True
+        self.async_trigger.set()
+        self.async_thread.join(self.timeout)
+        self.disconnect()
+
+    @property
+    def connected(self) -> bool:
+        return bool(self.sock)
+
+    def connect(self):
+        "connect to MPD"
+        if self.sock: return
+        self.log.info("connecting to %s:%d", self.ip, self.port)
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+            self.sock.settimeout(self.timeout)
+            self.sock.connect((self.ip, self.port))
+        except EnvironmentError as e:
+            self.log.error("connection failed - %s", str(e))
+            self.sock = None
+            return
+        res = dict(self._read_response())
+        if "OK" in res:
+            self.log.info("connected to %s", res['OK'])
+        else:
+            self.log.error("invalid response from server")
+            self.sock.close()
+            self.sock = None
+
+    def disconnect(self):
+        "disconnect from MPD"
+        if not self.sock: return
+        try:
+            self.sock.close()
+        except EnvironmentError:
+            pass
+        self.sock = None
+        self.log.info("disconnected from %s:%d", self.ip, self.port)
+
+    def _read_response(self, quiet: bool = False):
+        if not self.sock: return
+        buf = b''
+        end = False
+        while not end:
+            sep = buf.find(b'\n')
+            if sep < 0:
+                try:
+                    block = self.sock.recv(4096)
+                except EnvironmentError:
+                    self.disconnect()
+                    block = b''
+                if block:
+                    buf += block
+                    continue
+                else:
+                    sep = len(buf)
+                    end = True
+            line = buf[:sep].decode('utf-8', 'replace')
+            buf = buf[sep+1:]
+            if not quiet:
+                self.log.debug("RECV '%s'", line.rstrip())
+            if line.startswith("OK"):  yield ("OK",  line[2:].strip()); break
+            if line.startswith("ACK"): yield ("ACK", line[3:].strip()); break
+            try:
+                k, v = map(str.strip, line.split(':', 1))
+            except ValueError:
+                k, v = None, line
+            try:
+                v = int(v)
+            except ValueError:
+                try:
+                    v = float(v)
+                except ValueError:
+                    pass
+            yield (k, v)
+
+    def send_commands(self, *cmds, allow_reconnect: bool = True, quiet: bool = False) -> dict:
+        "send one or more commands synchronously; return last command's status"
+        if not self.sock:
+            self.connect()
+        with self.lock:
+            for cmd in cmds:
+                if self.cancel or not(self.sock):
+                    return {}
+                if not quiet:
+                    self.log.debug("SEND '%s'", cmd)
+                try:
+                    self.sock.sendall(cmd.encode('utf-8') + b'\n')
+                except EnvironmentError:
+                    self.disconnect()
+                    if allow_reconnect:
+                        return self.send_commands(*cmds, allow_reconnect=False, quiet=quiet)
+                res = dict(self._read_response(quiet=quiet))
+            return res
+
+    def send_commands_async(self, *cmds, quiet: bool = False):
+        "send one or more commands in a background thread"
+        self.get_async_result(wait=True)
+        self.async_result = None
+        self.async_cmds = cmds
+        self.async_quiet = quiet
+        self.async_done.clear()
+        self.async_trigger.set()
+
+    def get_async_result(self, wait: bool = True):
+        "get the result of the last asynchronously executed command"
+        if wait:
+            self.async_done.wait()
+        res = self.async_result
+        if res: self.async_result = None
+        return res
+
+    def _async_thread_func(self):
+        while not self.cancel:
+            self.async_trigger.wait()
+            self.async_trigger.clear()
+            if self.async_cmds:
+                cmds = self.async_cmds
+                self.async_cmds = None
+                self.async_result = self.send_commands(*cmds, quiet=self.async_quiet)
+            self.async_done.set()
+
+    @staticmethod
+    def shuffle_folders(*folders, single: bool = False):
+        "generate commands to play one or more folders in shuffled order"
+        return ['stop', 'clear', 'random 0'] \
+             +(['single 1', 'repeat 0'] if single else ['single 0', 'repeat 1']) \
+             + [f'add "{f}"' for f in folders] \
+             + ['shuffle', 'play']
+
+###############################################################################
+
+class MPDControl(Control):
+    "control widget to show status of an MPD client"
+
+    # button registry:    [0]       [1]        [2]         [3]       [4]            [5]
+    button_cmds        = ('play',   'pause 1', 'previous', 'next',   'seekcur -10', 'seekcur +10')
+    button_positions   = (2,        2,         0,          4,        1,             3)
+    button_icons       = ("\u23f5", "\u23f8",  "\u23ee",   "\u23ed", "\u23ea",      "\u23e9")
+    button_set_playing = (1,2,3,4,5)
+    button_set_paused  = (0,2,3,4,5)
+    buttons_that_navigate = (0,2,3)
+
+    def __init__(self, mpd: MPDClient, **style):
+        """
+        Instantiate the control with the following (mostly optional) parameters:
+        [* = can be different for each control state; ~ = in abstract size units]
+        - mpd = the MPDClient instance to control
+        - background  = background color
+        - font        = metadata text font
+        - size      ~ = default text size
+        - margin    ~ = outer margin
+        - icondist  ~ = distance between icon and text
+        - radius    ~ = rounding radius
+        - color       = text color
+        - icons       = title/artist/album icon color
+        - buttons     = button color
+        - time_color  = color of time and track number displays
+        - time_size   = relatize size of the time and track number displays
+        - alpha       = transparency of disabled buttons
+        - controls    = size (height) of the controls relative to the metadata
+                        text lines (1.0=same as text)
+        """
+        super().__init__(**style)
+        self.mpd = mpd
+        self.button_set = self.button_set_paused
+        self.next_update = 0
+        self.waiting_for_data = None
+        self.was_connected = False
+        self._on_connection_lost()
+
+    def _on_connection_lost(self):
+        self.text = [""] * 3
+        self.track = "\u2013/\u2013"
+        self.time = "\u2013/\u2013"
+        self.status_data = { 'status': {}, 'currentsong': {} }
+        self.curr_songid = -1
+
+    def do_layout(self, env: ControlEnvironment, x0: int, y0: int, x1: int, y1: int):
+        self.bg_color = color.parse(self.get('background', "111"))
+        self.bg_radius = env.scale(self.get('radius', 20))
+        self.c_text = color.parse(self.get('color', "fff"))
+        self.c_icon = color.parse(self.get('icons', "888"))
+        self.c_time = color.parse(self.get('time_color', "ccc"))
+        c = color.parse(self.get('buttons', "fff"))
+        self.c_buttons = [color.alpha(c, self.get('alpha', 0.5)), c]
+        base_size = env.scale(self.get('size', 50))
+        margin = env.scale(self.get('margin', 20))
+        x0 += margin
+        y0 += margin
+        x1 -= margin
+        y1 -= margin
+        row_height = int((y1 - y0) / (3 + self.get('controls', 1.0)))
+
+        # layout metadata fields
+        env.renderer.set_font("symbol")
+        self.icon_layout = []
+        for i, icon in enumerate("\u266B\u263A\u2680"):
+            self.icon_layout.extend(env.renderer.fit_text_in_box(
+                x0, y0 + i * row_height, x0 + row_height, y0 + i * row_height + row_height, base_size,
+                icon, 0,2
+            ))
+        env.renderer.set_font(self.get('font'))
+        text_x0 = max(x1 for x0,y0,x1,y1,sz,tx in self.icon_layout) \
+                + env.scale(self.get('icondist', 20))
+        self.text_layout = []
+        for item in self.text:
+            self.text_layout.extend(env.renderer.fit_text_in_box(
+                text_x0, y0, x1, y0 + row_height, base_size,
+                item, 0,2
+            ))
+            y0 += row_height
+
+        # layout buttons
+        env.renderer.set_font("symbol")
+        row_height = y1 - y0
+        bx0 = (x0 + x1 - 5 * row_height) // 2
+        self.button_rect = [(bx0 + i * row_height, y0, bx0 + (i + 1) * row_height, y1) for i in self.button_positions]
+        self.button_layout = [env.renderer.fit_text_in_box(*coords, row_height, icon)
+                              for coords, icon in zip(self.button_rect, self.button_icons)]
+
+        # layout track/time text
+        env.renderer.set_font(self.get('font'))
+        self.tt_size = int(min(row_height, base_size) * self.get('time_size', 0.75))
+        self.tt_x0 = x0
+        self.tt_x1 = x1
+        self.tt_y = (y0 + y1 - env.renderer.text_line_height(self.tt_size)) / 2
+
+    def do_draw(self, env: ControlEnvironment, x0: int, y0: int, x1: int, y1: int):
+        # process aynchronously requested data
+        next_frame_after = 1.0
+        if self.waiting_for_data:
+            #self.mpd.log.debug("waiting for %s", self.waiting_for_data)
+            res = self.mpd.get_async_result()
+            if res:
+                #self.mpd.log.debug("received %s", self.waiting_for_data)
+                slot = self.waiting_for_data
+                self.status_data[slot] = res
+                self.waiting_for_data = None
+                if slot == 'status':
+                    songid = res.get('songid', self.curr_songid)
+                    if songid != self.curr_songid:
+                        self.curr_songid = songid
+                        self._request_data('currentsong')
+                    track = self.status_data['status'].get('song', "\u2013")
+                    if isinstance(track, int): track = str(track + 1)
+                    self.track = track + " / " + \
+                                 str(self.status_data['status'].get('playlistlength', "\u2013"))
+                    elapsed = self.status_data['status'].get('elapsed')
+                    if elapsed:
+                        next_frame_after = 1.0 - (elapsed - int(elapsed))
+                    self.time = self._fmt_time(elapsed) + " / " + \
+                                self._fmt_time(self.status_data['status'].get('duration'))
+                    self.button_set = self.button_set_playing \
+                        if (self.status_data['status'].get('state') == 'play') \
+                        else self.button_set_paused
+                elif slot == 'currentsong':
+                    filename = os.path.splitext(os.path.basename(self.status_data['currentsong'].get('file', "")))[0]
+                    parts = [part.replace("_", " ").strip().title() for part \
+                             in re.split(r'-{2,}|[ _]-+[ _]', filename)]
+                    if len(parts) == 1:   fallback = [parts[0], "", ""]
+                    elif len(parts) == 2: fallback = [parts[1], parts[0], ""]
+                    else:                 fallback = [parts[2], parts[0], parts[1]]
+                    self.text = [self.status_data['currentsong'].get(k, fb) \
+                                 for k, fb in zip(('Title', 'Artist', 'Album'), fallback)]
+                    self.layout(env, x0,y0,x1,y1)
+        if self.was_connected and not(self.mpd.connected):
+            self._on_connection_lost()
+            self.layout(env, x0,y0,x1,y1)
+
+        # draw the control
+        env.renderer.box(x0, y0, x1, y1, self.bg_color, radius=self.bg_radius)
+        env.renderer.set_font(self.get('font'))
+        env.renderer.fitted_text(self.text_layout, self.c_text)
+        env.renderer.text_line(self.tt_x0, self.tt_y, self.tt_size, self.track, self.c_time)
+        env.renderer.text_line(self.tt_x1, self.tt_y, self.tt_size, self.time, self.c_time, align=1)
+        env.renderer.set_font('symbol')
+        env.renderer.fitted_text(self.icon_layout, self.c_icon)
+        c = self.c_buttons[int(self.mpd.connected)]
+        for i in self.button_set:
+            env.renderer.fitted_text(self.button_layout[i], c)
+
+        # schedule metadata update
+        if self.mpd.connected and (env.draw_time >= self.next_update):
+            self.next_update = env.draw_time + 0.1
+            self._request_data('status')
+        if self.waiting_for_data:
+            env.window.request_frames(1)
+        self.was_connected = self.mpd.connected
+        if self.mpd.connected:
+            return next_frame_after
+
+    def on_click(self, env: ControlEnvironment, x: int, y: int):
+        if not self.mpd.connected: return
+        for btn in self.button_set:
+            x0,y0, x1,y1 = self.button_rect[btn]
+            if (x0 <= x < x1) and (y0 <= y < y1):
+                self.send_commands(self.button_cmds[btn])
+                if btn in self.buttons_that_navigate:
+                    self.curr_songid = -1
+
+    def send_commands(self, *cmds):
+        "send commands to MPD and ensure that the UI state is refreshed immediately"
+        self.mpd.send_commands(*cmds)
+        self.next_update = 0
+
+    def _request_data(self, cmd_slot: str):
+        #self.mpd.log.debug("requesting %s", cmd_slot)
+        self.status_data[cmd_slot] = {}
+        self.mpd.send_commands_async(cmd_slot, quiet=True)
+        self.waiting_for_data = cmd_slot
+
+    @staticmethod
+    def _fmt_time(t):
+        if t is None: return "\u2013"
+        t = int(t)
+        return f"{t//60}:{t%60:02d}"
+
+###############################################################################
+
+if __name__ == "__main__":
+    import sys
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s | %(name)-24s | %(levelname)-8s | %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    mpd = MPDClient()
+    mpd.send_commands('update', 'clear', 'single 0', 'repeat 1', 'random 1', 'add "calm"', 'add "semicalm"', 'add "trance"', 'shuffle', 'play')
+    try:
+        songid = 0
+        while mpd.connected:
+            mpd.send_commands_async('status', quiet=True)
+            status = mpd.get_async_result()
+            if status.get('songid', songid) != songid:
+                songid = status['songid']
+                print("---------- NEW TRACK ----------")
+                mpd.send_commands_async('currentsong', quiet=True)
+                info =  mpd.get_async_result()
+                for k in ("file", "Title", "Artist", "Album", "Genre", "Date"):
+                    if k in info: print((k + ':').ljust(8) + str(info[k]))
+                continue
+            sys.stdout.write(f"{status.get('elapsed',0):.0f}/{status.get('duration',0):.0f}\r")
+            sys.stdout.flush()
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print()
+    mpd.send_commands('stop')
+    mpd.disconnect()
--- ctrlpad_test.py
+++ ctrlpad_test.py
@@ -12,6 +12,7 @@ from ctrlpad.renderer import Renderer
 from ctrlpad.controls import bind, merge_time, ControlEnvironment, \
                              GridLayout, TabSheet, Label, Button
 from ctrlpad.clock import Clock
+from ctrlpad.mpd import MPDClient, MPDControl
 from ctrlpad.crossbar import Crossbar
 
 
@@ -29,15 +30,39 @@ class MyApp(GLAppWindow):
         self.toplevel = TabSheet(toplevel=True, color="888")
         self.tl_clock_last_minute = 0
 
-        page = self.toplevel.add_page(GridLayout(16,8), "Page One", label="WELCOME")
+        # ---------------------------------------------------------------------
+
+        page = self.toplevel.add_page(GridLayout(16,8), "General", label="WELCOME")
         page.pack(8,8, Clock())
+        mpd = page.pack(8,3, MPDControl(MPDClient()))
+        page.locate(8,3)
+        page.pack(2,2, Button("Play BGM")).cmd = lambda e,b: \
+            mpd.send_commands(*MPDClient.shuffle_folders("calm", "semicalm", "trance"))
+        page.pack(2,2, Button("Play Demo-vibes")).cmd = lambda e,b: \
+            mpd.send_commands(*MPDClient.shuffle_folders("_mixes"))
+        page.pack(2,2, Button("Play Old-school")).cmd = lambda e,b: \
+            mpd.send_commands(*MPDClient.shuffle_folders("retro"))
+        page.pack(2,2, Button("Play Single Banger")).cmd = lambda e,b: \
+            mpd.send_commands(*MPDClient.shuffle_folders("banger", single=True))
+
+        # ---------------------------------------------------------------------
+
+        page = self.toplevel.add_page(GridLayout(16,8), "Examples", label="TESTING")
+        page.put(0,0, 12,1, Label("COLORFUL BUTTONS", valign=1, bar=3))
+        for i, name in enumerate("RED YELLOW GREEN CYAN BLUE MAGENTA".split()):
+            hue, sat = 30 + i * 60, 0.1
+            page.put(i*2,1, 2,2, Button(name, hue=hue, sat=sat, toggle=True))
+            page.put(i*2,3, 1,1, Button(name[0], hue=hue, sat=sat, state='disabled'))
+
+        page.locate(0,5)
         panic = page.pack(2,2, Button("PANIC BUTTON", state='disabled', hue=20, sat=.2))
         @bind(panic)
         def cmd(e,b):
-            panic.visible = False
+            mpd.mpd.send_commands('stop')
         page.pack(2,2, Button("CLICK")).cmd = lambda e,b: setattr(panic, 'state', None)
         page.pack(2,2, Button("TOGGLE", toggle=True)).cmd = lambda e,b: print("toggle state:", b.active)
-        page.locate(8,3)
+
+        page.locate(0,7)
         page.pack(1,1, Button("\u23ee", font="symbol"))  # prev
         page.pack(1,1, Button("\u23ea", font="symbol"))  # rewind
         page.pack(1,1, Button("\u23f5", font="symbol"))  # play
@@ -46,13 +71,9 @@ class MyApp(GLAppWindow):
         page.pack(1,1, Button("\u23cf", font="symbol"))  # eject
         page.pack(1,1, Button("\u23e9", font="symbol"))  # f.fwd
         page.pack(1,1, Button("\u23ed", font="symbol"))  # next
+        page.pack(8,1, Label("<- non-functional, illustrative only"))
 
-        page = self.toplevel.add_page(GridLayout(16,8), "Colors", label="COLORFUL")
-        page.put(0,0, 12,1, Label("COLORS", valign=1, bar=3))
-        for i, name in enumerate("RED YELLOW GREEN CYAN BLUE MAGENTA".split()):
-            hue, sat = 30 + i * 60, 0.1
-            page.put(i*2,1, 2,2, Button(name, hue=hue, sat=sat, toggle=True))
-            page.put(i*2,3, 1,1, Button(name[0], hue=hue, sat=sat, state='disabled'))
+        # ---------------------------------------------------------------------
 
         self.xbar = Crossbar(8, 8)
         # https://keyj.emphy.de/photos/deadline2023/dl23_videosetup.png
