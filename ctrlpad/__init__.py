69269e21b50a623a7621d843ed7f710c3d816bfe
Martin Fiedler <keyj@emphy.de>
2024-08-26 21:25:35 +0200

    provide a high-level application wrapper
    All the custom GLAppWindow class magic is now abstracted away;
    all the user needs to provide is a function that populates the
    top-level TabSheet control with pages and controls.

--- ctrlpad/__init__.py
+++ ctrlpad/__init__.py
@@ -1,2 +1,123 @@
 # SPDX-FileCopyrightText: 2024 Martin J. Fiedler <keyj@emphy.de>
 # SPDX-License-Identifier: MIT
+
+import argparse
+import logging
+import os
+import time
+
+__all__ = ['run_application']
+
+from .sdl import GLAppWindow, Cursor
+from .opengl import gl
+from .renderer import Renderer
+from ctrlpad.controls import merge_time, ControlEnvironment, TabSheet
+
+
+class CtrlPadAppWindow(GLAppWindow):
+    invisible_quit_button_size = 20
+
+    def on_init(self):
+        self.quit_timeout = None
+        self.tl_clock_last_minute = 0
+
+    def on_resize(self, old_w, old_h):
+        w, h = self.vp_width, self.vp_height
+        logging.info("screen resized to %dx%d", w, h)
+        self.env.update_scale()
+        self.env.toplevel.layout(self.env, 0,0, w, h)
+        self.request_frames(1)
+
+    def on_draw(self, t):
+        # update the clock
+        res = None
+        tm = time.localtime(t)
+        if tm.tm_min != self.tl_clock_last_minute:
+            self.env.toplevel.set_text(f"{tm.tm_hour}:{tm.tm_min:02d}")
+            self.tl_clock_last_minute = tm.tm_min
+        res = (60 - tm.tm_sec) + (1.0 - (t - int(t)))
+
+        # actual drawing
+        gl.Clear(gl.COLOR_BUFFER_BIT)
+        self.env.begin_frame(t)
+        res = merge_time(res, self.env.toplevel.draw(self.env))
+        self.env.end_frame()
+        return res
+
+    def on_key_down(self, sym: str):
+        if sym == 'Q': self.quit()
+
+    def on_mouse_down(self, x: int, y: int, button: int):
+        logging.debug("click @ %d,%d", x, y)
+        if (x > (self.vp_width - self.invisible_quit_button_size)) and (y < self.invisible_quit_button_size):
+            now = time.time()
+            if self.quit_timeout and (now < self.quit_timeout):
+                self.quit()
+            else:
+                logging.info("invisible quit button hit - double-click to quit the program")
+                self.quit_timeout = now + 0.5
+        else:
+            self.quit_timeout = None
+        self.env.toplevel.on_click(self.env, x, y)
+
+
+
+def run_application(title: str, init_func, **toplevel_kwargs):
+    """
+    High-level main function to run a control application.
+
+    This function takes care of all the gory details of command-line argument
+    parsing and application initialization, and provides a window with a
+    top-level TabSheet control. The init_func callback function has the
+    following signature:
+        init_func(env: ControlEnvironment)
+    The aforementioned top-level TabSheet control is available at env.toplevel.
+    The initialization function instantiates the entire UI and controllers,
+    and then run_application() takes over again and arranges for the main loop
+    to run.
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--fullscreen", action='store_true',
+                        help="run in fullscreen mode")
+    parser.add_argument("-c", "--no-cursor", action='store_true',
+                        help="don't show mouse cursor")
+    parser.add_argument("-r", "--fps-limit", metavar="FPS", type=float, default=0,
+                        help="set frame rate limit")
+    parser.add_argument("-g", "--geometry", metavar="WxH", default=(1024, 600),
+                        type=lambda s: tuple(map(int, s.lower().split('x', 1))),
+                        help="set initial window size")
+    parser.add_argument("-D", "--data-dir", metavar="DIR", default="data",
+                        help="set font data directory [default: %(default)s]")
+    parser.add_argument("-F", "--primary-font", metavar="NAME", default="bahn",
+                        help="set primary UI font [default: %(default)s]")
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s | %(name)-20s | %(levelname)-8s | %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    app = CtrlPadAppWindow(*args.geometry, title, fullscreen=args.fullscreen, fps_limit=args.fps_limit)
+    app.env = ControlEnvironment(app, Renderer())
+    app.env.renderer.add_font(os.path.join(args.data_dir, args.primary_font))
+    if args.primary_font != "symbol":
+        app.env.renderer.add_font(os.path.join(args.data_dir, "symbol"))
+
+    final_tl_kwargs = {'color': "888"}
+    final_tl_kwargs.update(toplevel_kwargs)
+    app.env.toplevel = TabSheet(toplevel=True, **final_tl_kwargs)
+
+    app.set_cursor(Cursor.Hand)
+    if args.no_cursor:
+        app.hide_cursor()
+
+    init_func(app.env)
+    app.env.toplevel.layout(app.env, 0,0, app.vp_width, app.vp_height)
+
+    logging.info("initialization finished, starting main loop")
+    app.main_loop()
+    logging.info("quitting ...")
+    del app
+    logging.info("application exited")
--- ctrlpad_test.py
+++ ctrlpad_test.py
@@ -2,156 +2,75 @@
 # SPDX-FileCopyrightText: 2024 Martin J. Fiedler <keyj@emphy.de>
 # SPDX-License-Identifier: MIT
 
-import argparse
-import logging
-import time
-
-from ctrlpad.sdl import GLAppWindow, Cursor
-from ctrlpad.opengl import gl
-from ctrlpad.renderer import Renderer
-from ctrlpad.controls import bind, merge_time, ControlEnvironment, \
-                             GridLayout, TabSheet, Label, Button
+import ctrlpad
+from ctrlpad.controls import bind, ControlEnvironment, GridLayout, Label, Button
 from ctrlpad.clock import Clock
 from ctrlpad.mpd import MPDClient, MPDControl
 from ctrlpad.crossbar import Crossbar
 
 
-class MyApp(GLAppWindow):
-    invisible_quit_button_size = 20
+def init_app(env: ControlEnvironment):
 
-    def on_init(self):
-        self.renderer = Renderer()
-        self.renderer.add_font("data/bahn")
-        self.renderer.add_font("data/symbol", baseline_shift=-0.1)
-        self.set_cursor(Cursor.Hand)
-        self.quit_timeout = None
+    page = env.toplevel.add_page(GridLayout(16,8), "General", label="WELCOME")
+    page.pack(8,8, Clock())
+    mpd = page.pack(8,3, MPDControl(MPDClient()))
+    page.locate(8,3)
+    page.pack(2,2, Button("Play BGM")).cmd = lambda e,b: \
+        mpd.send_commands(*MPDClient.shuffle_folders("calm", "semicalm", "trance"))
+    page.pack(2,2, Button("Play Demo-vibes")).cmd = lambda e,b: \
+        mpd.send_commands(*MPDClient.shuffle_folders("_mixes"))
+    page.pack(2,2, Button("Play Old-school")).cmd = lambda e,b: \
+        mpd.send_commands(*MPDClient.shuffle_folders("retro"))
+    page.pack(2,2, Button("Play Single Banger")).cmd = lambda e,b: \
+        mpd.send_commands(*MPDClient.shuffle_folders("banger", single=True))
 
-        self.env = ControlEnvironment(self, self.renderer)
-        self.toplevel = TabSheet(toplevel=True, color="888")
-        self.tl_clock_last_minute = 0
+    # ---------------------------------------------------------------------
 
-        # ---------------------------------------------------------------------
+    page = env.toplevel.add_page(GridLayout(16,8), "Examples", label="TESTING")
+    page.put(0,0, 12,1, Label("COLORFUL BUTTONS", valign=1, bar=3))
+    for i, name in enumerate("RED YELLOW GREEN CYAN BLUE MAGENTA".split()):
+        hue, sat = 30 + i * 60, 0.1
+        page.put(i*2,1, 2,2, Button(name, hue=hue, sat=sat, toggle=True))
+        page.put(i*2,3, 1,1, Button(name[0], hue=hue, sat=sat, state='disabled'))
 
-        page = self.toplevel.add_page(GridLayout(16,8), "General", label="WELCOME")
-        page.pack(8,8, Clock())
-        mpd = page.pack(8,3, MPDControl(MPDClient()))
-        page.locate(8,3)
-        page.pack(2,2, Button("Play BGM")).cmd = lambda e,b: \
-            mpd.send_commands(*MPDClient.shuffle_folders("calm", "semicalm", "trance"))
-        page.pack(2,2, Button("Play Demo-vibes")).cmd = lambda e,b: \
-            mpd.send_commands(*MPDClient.shuffle_folders("_mixes"))
-        page.pack(2,2, Button("Play Old-school")).cmd = lambda e,b: \
-            mpd.send_commands(*MPDClient.shuffle_folders("retro"))
-        page.pack(2,2, Button("Play Single Banger")).cmd = lambda e,b: \
-            mpd.send_commands(*MPDClient.shuffle_folders("banger", single=True))
+    page.locate(0,5)
+    panic = page.pack(2,2, Button("PANIC BUTTON", state='disabled', hue=20, sat=.2))
+    @bind(panic)
+    def cmd(e,b):
+        mpd.mpd.send_commands('stop')
+    page.pack(2,2, Button("CLICK")).cmd = lambda e,b: setattr(panic, 'state', None)
+    page.pack(2,2, Button("TOGGLE", toggle=True)).cmd = lambda e,b: print("toggle state:", b.active)
 
-        # ---------------------------------------------------------------------
+    page.locate(0,7)
+    page.pack(1,1, Button("\u23ee", font="symbol"))  # prev
+    page.pack(1,1, Button("\u23ea", font="symbol"))  # rewind
+    page.pack(1,1, Button("\u23f5", font="symbol"))  # play
+    page.pack(1,1, Button("\u23f8", font="symbol"))  # pause
+    page.pack(1,1, Button("\u23f9", font="symbol"))  # stop
+    page.pack(1,1, Button("\u23cf", font="symbol"))  # eject
+    page.pack(1,1, Button("\u23e9", font="symbol"))  # f.fwd
+    page.pack(1,1, Button("\u23ed", font="symbol"))  # next
+    page.pack(8,1, Label("<- non-functional, illustrative only"))
 
-        page = self.toplevel.add_page(GridLayout(16,8), "Examples", label="TESTING")
-        page.put(0,0, 12,1, Label("COLORFUL BUTTONS", valign=1, bar=3))
-        for i, name in enumerate("RED YELLOW GREEN CYAN BLUE MAGENTA".split()):
-            hue, sat = 30 + i * 60, 0.1
-            page.put(i*2,1, 2,2, Button(name, hue=hue, sat=sat, toggle=True))
-            page.put(i*2,3, 1,1, Button(name[0], hue=hue, sat=sat, state='disabled'))
+    # ---------------------------------------------------------------------
 
-        page.locate(0,5)
-        panic = page.pack(2,2, Button("PANIC BUTTON", state='disabled', hue=20, sat=.2))
-        @bind(panic)
-        def cmd(e,b):
-            mpd.mpd.send_commands('stop')
-        page.pack(2,2, Button("CLICK")).cmd = lambda e,b: setattr(panic, 'state', None)
-        page.pack(2,2, Button("TOGGLE", toggle=True)).cmd = lambda e,b: print("toggle state:", b.active)
-
-        page.locate(0,7)
-        page.pack(1,1, Button("\u23ee", font="symbol"))  # prev
-        page.pack(1,1, Button("\u23ea", font="symbol"))  # rewind
-        page.pack(1,1, Button("\u23f5", font="symbol"))  # play
-        page.pack(1,1, Button("\u23f8", font="symbol"))  # pause
-        page.pack(1,1, Button("\u23f9", font="symbol"))  # stop
-        page.pack(1,1, Button("\u23cf", font="symbol"))  # eject
-        page.pack(1,1, Button("\u23e9", font="symbol"))  # f.fwd
-        page.pack(1,1, Button("\u23ed", font="symbol"))  # next
-        page.pack(8,1, Label("<- non-functional, illustrative only"))
-
-        # ---------------------------------------------------------------------
-
-        self.xbar = Crossbar(8, 8)
-        # https://keyj.emphy.de/photos/deadline2023/dl23_videosetup.png
-        self.xbar.add_ui_page(self.toplevel, input_names={
-            '1': "ATEM OUT 2",
-            '2': "Stream Output",
-            '3': "FOH HDMI",
-            '4': "Old school",
-            '5': "Compo1\n",
-            '6': "Compo2\n",
-            '7': "Screens\n",
-            '8': "n/c\n"
-        }, output_names=[
-            "ATEM IN 1", "ATEM IN 2", "ATEM IN 3", "Stream Team",
-            "Compo1 Monitor", "Compo2 Monitor",
-            "Main Screen", "Bar Screen"
-        ], input_format="\u203a\u2039", output_format="\u2039\u203a")
-
-        self.toplevel.layout(self.env, 0,0, self.vp_width, self.vp_height)
-
-    def on_resize(self, old_w, old_h):
-        w, h = self.vp_width, self.vp_height
-        logging.info("screen resized to %dx%d", w, h)
-        self.env.update_scale()
-        self.toplevel.layout(self.env, 0,0, w, h)
-        self.request_frames(1)
-
-    def on_draw(self, t):
-        # update the clock
-        res = None
-        tm = time.localtime(t)
-        if tm.tm_min != self.tl_clock_last_minute:
-            self.toplevel.set_text(f"{tm.tm_hour}:{tm.tm_min:02d}")
-            self.tl_clock_last_minute = tm.tm_min
-        res = (60 - tm.tm_sec) + (1.0 - (t - int(t)))
-
-        # actual drawing
-        gl.Clear(gl.COLOR_BUFFER_BIT)
-        self.env.begin_frame(t)
-        res = merge_time(res, self.toplevel.draw(self.env))
-        self.env.end_frame()
-        return res
-
-    def on_key_down(self, sym: str):
-        if sym == 'Q': self.quit()
-
-    def on_mouse_down(self, x: int, y: int, button: int):
-        logging.debug("click @ %d,%d", x, y)
-        if (x > (self.vp_width - self.invisible_quit_button_size)) and (y < self.invisible_quit_button_size):
-            now = time.time()
-            if self.quit_timeout and (now < self.quit_timeout):
-                self.quit()
-            else:
-                logging.info("invisible quit button hit - double-click to quit the program")
-                self.quit_timeout = now + 0.5
-        else:
-            self.quit_timeout = None
-        self.toplevel.on_click(self.env, x, y)
+    xbar = Crossbar(8, 8)
+    # https://keyj.emphy.de/photos/deadline2023/dl23_videosetup.png
+    xbar.add_ui_page(env.toplevel, input_names={
+        '1': "ATEM OUT 2",
+        '2': "Stream Output",
+        '3': "FOH HDMI",
+        '4': "Old school",
+        '5': "Compo1\n",
+        '6': "Compo2\n",
+        '7': "Screens\n",
+        '8': "n/c\n"
+    }, output_names=[
+        "ATEM IN 1", "ATEM IN 2", "ATEM IN 3", "Stream Team",
+        "Compo1 Monitor", "Compo2 Monitor",
+        "Main Screen", "Bar Screen"
+    ], input_format="\u203a\u2039", output_format="\u2039\u203a")
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(asctime)s | %(name)-20s | %(levelname)-8s | %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    )
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--fullscreen", action='store_true',
-                        help="run in fullscreen mode")
-    parser.add_argument("-c", "--no-cursor", action='store_true',
-                        help="don't show mouse cursor")
-    parser.add_argument("-r", "--fps-limit", type=float, default=0,
-                        help="set frame rate limit")
-    args = parser.parse_args()
-
-    app = MyApp(1024, 600, "ControlPad Test App", fullscreen=args.fullscreen, fps_limit=args.fps_limit)
-    if args.no_cursor: app.hide_cursor()
-    app.main_loop()
-    logging.info("quitting ...")
-    del app
-    logging.info("program exited")
+    ctrlpad.run_application("ControlPad Test App", init_app)
